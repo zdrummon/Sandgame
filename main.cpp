1cf627b05f0cedd614a0807c1b099ac4cfc0ed32
Zachary <zdrummon@gmail.com>
2022-11-10 06:54:25 +0000

    added better vertex data and a buffer

--- main.cpp
+++ main.cpp
@@ -1,16 +1,6 @@
 /* 
-  ██████  ▄▄▄       ███▄    █ ▓█████▄   ▄████  ▄▄▄       ███▄ ▄███▓▓█████ 
-▒██    ▒ ▒████▄     ██ ▀█   █ ▒██▀ ██▌ ██▒ ▀█▒▒████▄    ▓██▒▀█▀ ██▒▓█   ▀ 
-░ ▓██▄   ▒██  ▀█▄  ▓██  ▀█ ██▒░██   █▌▒██░▄▄▄░▒██  ▀█▄  ▓██    ▓██░▒███   
-  ▒   ██▒░██▄▄▄▄██ ▓██▒  ▐▌██▒░▓█▄   ▌░▓█  ██▓░██▄▄▄▄██ ▒██    ▒██ ▒▓█  ▄ 
-▒██████▒▒ ▓█   ▓██▒▒██░   ▓██░░▒████▓ ░▒▓███▀▒ ▓█   ▓██▒▒██▒   ░██▒░▒████▒
-▒ ▒▓▒ ▒ ░ ▒▒   ▓▒█░░ ▒░   ▒ ▒  ▒▒▓  ▒  ░▒   ▒  ▒▒   ▓▒█░░ ▒░   ░  ░░░ ▒░ ░
-░ ░▒  ░ ░  ▒   ▒▒ ░░ ░░   ░ ▒░ ░ ▒  ▒   ░   ░   ▒   ▒▒ ░░  ░      ░ ░ ░  ░
-░  ░  ░    ░   ▒      ░   ░ ░  ░ ░  ░ ░ ░   ░   ░   ▒   ░      ░      ░   
-      ░        ░  ░         ░    ░          ░       ░  ░       ░      ░  ░                                         
-created by:                    ░
-Trevor Haggerty  | cesismalon@gmail.com
-Zachary Drummond | zdrummon@gmail.com 
+Sandgame
+created by: Trevor Haggerty - cesismalon@gmail.com & Zachary Drummond - zdrummon@gmail.com 
 */
 
 /* glew needs to be before any other opengl include */
@@ -22,21 +12,21 @@ using std::cout; using std::endl;
 
 int main(void)
 {
-    cout << "███████████████████████████████████████████████████████████████████████████████" << endl;
-    cout << "█                                                                             █" << endl;
-    cout << "█   ██████  ▄▄▄       ███▄    █ ▓█████▄   ▄████  ▄▄▄       ███▄ ▄███▓▓█████   █" << endl;
-    cout << "█ ▒██    ▒ ▒████▄     ██ ▀█   █ ▒██▀ ██▌ ██▒ ▀█▒▒████▄    ▓██▒▀█▀ ██▒▓█   ▀   █" << endl;
-    cout << "█ ░ ▓██▄   ▒██  ▀█▄  ▓██  ▀█ ██▒░██   █▌▒██░▄▄▄░▒██  ▀█▄  ▓██    ▓██░▒███     █" << endl;
-    cout << "█   ▒   ██▒░██▄▄▄▄██ ▓██▒  ▐▌██▒░▓█▄   ▌░▓█  ██▓░██▄▄▄▄██ ▒██    ▒██ ▒▓█  ▄   █" << endl;
-    cout << "█ ▒██████▒▒ ▓█   ▓██▒▒██░   ▓██░░▒████▓ ░▒▓███▀▒ ▓█   ▓██▒▒██▒   ░██▒░▒████▒  █" << endl;
-    cout << "█ ▒ ▒▓▒ ▒ ░ ▒▒   ▓▒█░░ ▒░   ▒ ▒  ▒▒▓  ▒  ░▒   ▒  ▒▒   ▓▒█░░ ▒░   ░  ░░░ ▒░ ░  █" << endl;
-    cout << "█ ░ ░▒  ░ ░  ▒   ▒▒ ░░ ░░   ░ ▒░ ░ ▒  ▒   ░   ░   ▒   ▒▒ ░░  ░      ░ ░ ░  ░  █" << endl;
-    cout << "█ ░  ░       ░   ▒      ░   ░ ░  ░ ░  ░ ░ ░   ░   ░   ▒   ░      ░      ░     █" << endl;
-    cout << "█       ░    ░   ░  ░         ░    ░   ░      ░       ░  ░       ░      ░  ░  █" << endl;
-    cout << "█ created by ░   ░░               ░    ░      ░░                ░░░    ░░░    █" << endl;
-    cout << "█ Trevor Haggerty cesismalon@gmail.com ░░ Zachary Drummond zdrummon@gmail.com █" << endl;
-    cout << "█░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█" << endl;
-    cout << "███████████████████████████████████████████████████████████████████████████████" << endl;
+    cout << "██████████████████████████████████████████████████████████████████████████████" << endl;
+    cout << "█                                                                            █" << endl;
+    cout << "█   ██████  ▄▄▄       ███▄    █ ▓█████▄   ▄████  ▄▄▄       ███▄ ▄███▓▓█████  █" << endl;
+    cout << "█ ▒██    ▒ ▒████▄     ██ ▀█   █ ▒██▀ ██▌ ██▒ ▀█▒▒████▄    ▓██▒▀█▀ ██▒▓█   ▀  █" << endl;
+    cout << "█ ░ ▓██▄   ▒██  ▀█▄  ▓██  ▀█ ██▒░██   █▌▒██░▄▄▄░▒██  ▀█▄  ▓██    ▓██░▒███    █" << endl;
+    cout << "█   ▒   ██▒░██▄▄▄▄██ ▓██▒  ▐▌██▒░▓█▄   ▌░▓█  ██▓░██▄▄▄▄██ ▒██    ▒██ ▒▓█  ▄  █" << endl;
+    cout << "█ ▒██████▒▒ ▓█   ▓██▒▒██░   ▓██░░▒████▓ ░▒▓███▀▒ ▓█   ▓██▒▒██▒   ░██▒░▒████▒ █" << endl;
+    cout << "█ ▒ ▒▓▒ ▒ ░ ▒▒   ▓▒█░░ ▒░   ▒ ▒  ▒▒▓  ▒  ░▒   ▒  ▒▒   ▓▒█░░ ▒░   ░  ░░░ ▒░ ░ █" << endl;
+    cout << "█ ░ ░▒  ░ ░  ▒   ▒▒ ░░ ░░   ░ ▒░ ░ ▒  ▒   ░   ░   ▒   ▒▒ ░░  ░      ░ ░ ░  ░ █" << endl;
+    cout << "█ ░  ░       ░   ▒      ░   ░ ░  ░ ░  ░ ░ ░   ░   ░   ▒   ░      ░      ░    █" << endl;
+    cout << "█       ░    ░   ░  ░         ░    ░   ░      ░       ░  ░       ░      ░  ░ █" << endl;
+    cout << "█ created by ░   ░░               ░    ░      ░░                ░░░    ░░░   █" << endl;
+    cout << "█ Trevor Haggerty cesismalon@gmail.com ░ Zachary Drummond zdrummon@gmail.com █" << endl;
+    cout << "█░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█" << endl;
+    cout << "██████████████████████████████████████████████████████████████████████████████" << endl;
 
     GLFWwindow* window;
 
@@ -68,17 +58,30 @@ int main(void)
 
     cout << "OPENGL " << glGetString(GL_VERSION) << endl;
 
+    /* vertex position array */
+    const int POSITION_SIZE = 6;
+    float positions[POSITION_SIZE] = {
+        -0.5f, -0.5f,
+        0.0f, 0.5f,
+        0.5f, -0.5f
+    };
+
+    /* Generate a buffer (number of buffers, pointer to unsigned int), (buffer type, choose buffer) */
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+
+    /* data to put into the buffer */
+    glBufferData(GL_ARRAY_BUFFER, POSITION_SIZE * sizeof(float), positions, GL_STATIC_DRAW);
+
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f(0.0f, 0.5f);
-        glVertex2f(0.5f, -0.5f);
-        glEnd();
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
